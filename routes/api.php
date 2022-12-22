8704266abd1b3b48f6c9a91d3e188fd1d2443f95
jhonVargasT <jhonvargast@gmail.com>
2022-12-22 18:42:57 -0500

    proyecto y formularios agragados

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassportController;
+use App\Http\Controllers\ProyectoController;
+use App\Http\Controllers\FormularioController;
+
+
 
 Route::post('register', [PassportController::class, 'register']);
 Route::post('login', [PassportController::class, 'login']);
 
 //pantalla de incio del proyecto
+Route::get('portadaProyecto',[ProyectoController::class,'getProyecto']);
+Route::get('formulario',[FormularioController::class,'getFormulario']);
 
 // put all api protected routes here
 Route::middleware('auth:api')->group(function () {
